47ae6c6fd846fb4a05b404c9c258585f539a2b22
Andy <43708848+andriilive@users.noreply.github.com>
2022-06-10 13:19:18 +0200

    1

--- src/app.js
+++ src/app.js
@@ -3,125 +3,45 @@ import {gsap, ScrollTrigger, Observer, ScrollToPlugin, CSSPlugin} from "gsap/all
 
 gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, CSSPlugin);
 
-$(document).ready(function () {
+// GASP NOT JQUERY ELEMENTS
+const $canvas = $('#sections');
+const canvas = $canvas[0];
 
-    gsap.to(window, {duration: 2, scrollTo: "html"});
+const sections = gsap.utils.toArray('.section');
+const sectionsButton = $('#sections-btn')[0];
+const $circle = $('#sections-graphics');
+const circle = $circle[0];
 
-    // GASP NOT JQUERY ELEMENTS
-    const $canvas = $('#sections');
-    const canvas = $canvas[0];
+$canvas.css({'--bg-color': '#000'}).data('sections', sections.length);
 
-    const sections = gsap.utils.toArray('.section');
-    const sectionsButton = $('#sections-btn')[0];
-    const $circle = $('#sections-graphics');
-    const circle = $circle[0];
+ScrollTrigger.defaults({
+    markers: false
+});
 
-    $canvas.css({'--bg-color': '#000'}).data('sections', sections.length);
+let animation = gsap.timeline();
 
-    // Defaults
-    function set_defaults() {
-        // gsap.set(sectionsButton, {autoAlpha: 0, y: 500});
-        // gsap.set(circle, {autoAlpha: 0, scale: 0});
-    }
+ScrollTrigger.create({
+    trigger: canvas,
+    id: 'canvas',
+    pin: true,
+});
 
-    set_defaults();
 
-    ScrollTrigger.defaults({
-        markers: true
+gsap.utils.toArray('.section').forEach(function (section, i) {
+
+    const activeSection = i - 1;
+
+    let timeline = gsap.timeline();
+
+    ScrollTrigger.create({
+        trigger: section,
+        pin: true,
+        scrub: 1,
+        animation: timeline
     });
 
-    const canvasScrollTrigger = ScrollTrigger.create({
-        trigger: canvas,
-        pin: '#sections-wrap',
-        start: 'top top',
-        end: 'bottom bottom',
-        toggleClass: 'j-active',
-        scrub: true,
-        markers: false,
-        snapTo: 1 / (sections.length - 1),
-        id: "sections",
-        onToggle: self => {
-            console.log('canvasOnToggle', self.isActive)
-        },
-        onUpdate: self => {
-            const {progress} = self;
-        },
-        onLeave: self => {
-
-        }
-    });
-
-    sections.forEach(function (thisSection, i) {
-        console.log(i, thisSection, thisSection.nextElementSibling);
-
-        // gsap.set(thisSection, {height: '250vh'});
-
-        // let anim = gsap.timeline();
-        //
-        // ScrollTrigger.create({
-        //     trigger: thisSection,
-        //     pin: thisSection,
-        //     start: 'top top',
-        //     end: 'bottom bottom',
-        //     scrub: 0,
-        //     animation: anim
-        // });
-        //
-        // anim.to(thisSection, {autoAlpha: 0});
-
-        const sectionsTimeline = gsap.timeline(/*{paused: true}*/);
-
-        const sectionsScrollTrigger = ScrollTrigger.create({
-            trigger: thisSection,
-            start: 'top center',
-            end: 'bottom center',
-            toggleClass: 'j-active',
-            scrub: true,
-            snap: {
-                snapTo: 1 / 5
-            },
-            animation: sectionsTimeline,
-            id: "section-" + i,
-            onToggle: self => {
-
-                const {isActive, direction} = self;
-                const $circle = $(circle);
-
-                if (isActive) {
-                    window.activeSection = i + 1;
-                    // if true -> to right || if false <- to left
-                    if (activeSection % 2 === 0) {
-
-                    } else {
-                        // sectionsTimeline.to(circle, {x: '100%'})
-                    }
-                }
-
-                sectionsTimeline.fromTo(circle, {x: 0}, {x: '100%'});
-
-
-                // 1 | 1
-
-                // console.log('sectionOnToggle', self, circleDirection)
-            },
-            onUpdate: self => {
-                const {progress} = self;
-                // console.log('sectionProgress', progress)
-            },
-        });
-
-        ScrollTrigger.create({
-            target: thisSection,
-            id: "section-" + i,
-            pin: true,
-            top: 'top top',
-            start: 'top bottom',
-            onEnter: self => {
-
-            }
-            // containerAnimation: containerAnimation,
-        });
-
-    })
+    timeline
+        .from(section, {autoAlpha: 0})
+        .to(section, {autoAlpha: 0});
 
 });
