90a5e1b8e0adcb755d57fc8fd6f2ebdc9e320e4a
Andy <43708848+andriilive@users.noreply.github.com>
2022-06-13 17:05:12 +0200

    default sections trigger + nav function

--- src/app.js
+++ src/app.js
@@ -3,60 +3,46 @@ import {gsap, ScrollTrigger, Observer, ScrollToPlugin, CSSPlugin} from "gsap/all
 
 gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, CSSPlugin);
 
+ScrollTrigger.defaults({
+    markers: true
+});
+
 $(document).ready(function () {
+    const $canvas = $('.sections');
+    const canvas = $canvas[0];
+    const canvasTimeline = gsap.timeline({paused: true});
+
+    const circle = $('#sections-graphics')[0];
+    const sectionsButton = $('#sections-btn')[0];
+
+    function toggle_nav(state) {
+        if (state) {
+            console.log('enable navbar');
+        } else {
+            console.log('disable navbar');
+        }
+    }
+
+    gsap.set(sectionsButton, {bottom: -100});
+    gsap.set(circle, {scale: 0.7});
+
+    ScrollTrigger.create({
+        id: 'canvas',
+        trigger: canvas,
+        start: 'top top',
+        end: 'bottom bottom',
+        toggleClass: 'j-active',
+        onToggle: self => {
+            const {direction, isActive, progress} = self;
+            // console.log('CanvasonToggle',direction, isActive, progress, self);
+            toggle_nav(isActive);
+        }
+    })
+
 
     const $sections = $('#sections');
     const sections = gsap.utils.toArray('.section');
-    const section = gsap.utils.selector('.section');
-
-    const circle = gsap.utils.selector('#sections-graphics');
-    const sectionsButton = gsap.utils.selector('#sections-btn');
-
     const sectionsTimeline = gsap.timeline();
-
-    // Defaults
-    gsap.set(sectionsButton, {});
-
-    // let scrollTween = gsap.to(sections, {
-    //
-    //     scrollTrigger: {
-    //         trigger: "#sections",
-    //         markers: true,
-    //         animation: sectionsTimeline,
-    //         id: '#sections',
-    //         pin: true,
-    //         snap: {
-    //             delay: 0,
-    //             snapTo: 1 / (sections.length - 1)
-    //         },
-    //         scrub: 0,
-    //
-    //         onEnter: (self) => {
-    //             console.log("onEnter", self);
-    //
-    //             gsap.to(sectionsButton, {y: false, autoAlpha: 1})
-    //         },
-    //         onLeave: (self) => console.log("onLeave", self),
-    //
-    //         onUpdate: (self) => {
-    //             // console.log('onUpdate', self)
-    //         },
-    //         onToggle: (self) => {
-    //             console.log("onToggle", self, {active: self.isActive})
-    //         },
-    //         onScrubComplete: (self) => {
-    //             console.log('onScrubComplete', self)
-    //         },
-    //         onSnapComplete: (self) => {
-    //             console.log('onSnapComplete', self)
-    //         }
-    //
-    //         // onEnterBack: () => console.log("enterBack"),
-    //         // onLeaveBack: () => console.log("leaveBack"),
-    //     }
-    // });
-
-    // sectionsTimeline.addLabel("intro-visible").to(sectionsButton, {autoAlpha: 1, y: 0});
 
     $sections.css({'--bg-color': '#000'}).data('sections', sections.length);
 });
